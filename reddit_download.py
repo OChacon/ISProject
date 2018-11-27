7053de72e5df24a9a1a01c06980b00f5dac97f80
MikHail <mxh5594@rit.edu>
2018-11-26 20:46:31 -0500

    dual subreddit set up and change subreddits

--- reddit_download.py
+++ reddit_download.py
@@ -3,10 +3,19 @@ import sys
 import json
 from reference import reddit
 
-def download(subreddit, outfile, count):
+def download(subredditA, subredditB, outfile, count):
     #p = reddit.subreddit('tifu').random()
     pd = {}
-    for post in [reddit.subreddit(subreddit).random() for x in range(count)]:
+    for post in [reddit.subreddit(subredditA).random() for x in range(count)]:
+        p = {}
+        p['title'] = post.title
+        p['desc'] = post.selftext
+        p['topComment'] = ""
+        if len(post.comments) > 0:
+            p['topComment'] = post.comments[0].body
+        p['subReddit'] = post.subreddit.display_name
+        pd[post.id] = p
+    for post in [reddit.subreddit(subredditB).random() for x in range(count)]:
         p = {}
         p['title'] = post.title
         p['desc'] = post.selftext
@@ -19,4 +28,4 @@ def download(subreddit, outfile, count):
     json.dump(pd,of,indent=4,separators=(',', ': '))
 
 if __name__ == '__main__':
-    download('tifu','tifu_10.json',10)
+    download('legaladvice','personalfinance','la_pf_10.json',10)
