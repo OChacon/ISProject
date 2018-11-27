53ae57f928f2c5f5fc4b475f0a0f01c009a1496c
MikHail <mxh5594@rit.edu>
2018-11-26 20:25:19 -0500

    cleaner downloadSetup and JSON dump

--- reddit_download.py
+++ reddit_download.py
@@ -1,10 +1,22 @@
 import praw
+import sys
+import json
 from reference import reddit
-#p = reddit.subreddit('tifu').random()
 
-for post in [reddit.subreddit('tifu').random() for x in range(100)]:
-    p = {}
-    p['title'] = post.title
-    p['desc'] = post.selftext
-    p['topComment'] = post.comments[0].body
-    post.count(0)
+def download(subreddit, outfile, count):
+    #p = reddit.subreddit('tifu').random()
+    pd = {}
+    for post in [reddit.subreddit(subreddit).random() for x in range(count)]:
+        p = {}
+        p['title'] = post.title
+        p['desc'] = post.selftext
+        p['topComment'] = ""
+        if len(post.comments) > 0:
+            p['topComment'] = post.comments[0].body
+        p['subReddit'] = post.subreddit.display_name
+        pd[post.id] = p
+    of = open(outfile, 'w+')
+    json.dump(pd,of,indent=4,separators=(',', ': '))
+
+if __name__ == '__main__':
+    download('tifu','tifu_10.json',10)
